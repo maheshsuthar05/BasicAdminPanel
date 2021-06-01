5866f178c764e23634ac65622fdebcf1f9592233
Mahesh Suthar <79853792+maheshsuthar05@users.noreply.github.com>
2021-06-01 20:37:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function getDatafromAPI() {
     return new Promise((resolve, reject) => {
         $.ajax({
             type: "GET",
-            url: "http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D",
+            url: "https://60b6386d8ac0790017829a00.mockapi.io/BasicAdminPanel",
             success: function (response) {
                 personData = response;
                 resolve();
@@ -48,7 +48,6 @@ function renderTableData(personData, tableBody) {
         $("#search-box").on('keyup', function(){
             var value = $(this).val().toLowerCase();
             $(".data-row").each(function () {
-                console.log($(this).text().toLowerCase().search(value));
                if ($(this).text().toLowerCase().search(value) > -1) {
                   $(this).show();
                   $(this).prev('.subjectName').last().show();
